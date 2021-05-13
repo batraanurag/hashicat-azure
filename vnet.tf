d1b4fdc7f6adb7362d4f9f926a00d87249599140
Anurag Batra <batra.anurag@gmail.com>
2021-05-13 22:21:48 +0000

    Added vnet module4

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/abatraIB/network/azurerm"
   version = "3.0.1"
-  resource_group_name = "myresourcegroup"
+  resource_group_name = "abat-workshop"
   # insert required variables here
 }
