625856c49e5e931dbb4848e1304a390f6314cfc0
Anurag Batra <batra.anurag@gmail.com>
2021-05-13 21:29:28 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -122,7 +122,9 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {
+    Billable = "true"
+  }
 }
 
 # We're using a little trick here so we can run the provisioner without
